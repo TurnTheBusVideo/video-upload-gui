536b0a01b35da0277f27dbac6e320e31a1dd1849
Prasad Thakur <prasad.thakur@sap.com>
2020-01-19 15:28:22 -0800

    script wiring

--- upload.js
+++ upload.js
@@ -1,33 +1,31 @@
 window.onload = event => {
     $('#uploadMask').attr('hidden', true)
-    $.ajax({
-        type: "GET",
-        url: "https://6bvu1dfzvl.execute-api.ap-south-1.amazonaws.com/test/getsignedurl",
-        crossdomain: true,
-        contentType: 'application/json',
-        dataType: 'json',
-        success: function (data, textStatus, jqXHR) {
-            let signedURL = JSON.parse(data.body).signedURL
-            let formData = $('#file').val()
-            $.ajax({
-                type: "PUT",
-                url: signedURL,
-                contentType: "application/octet-stream",
-                data: formData,
-                success: function (data, textStatus, jqXHR) {
-                    alert('everything was OK')
-                }
-            });
-        }
-    });
-
 
     $('#submitUploadForm').click(event => {
         $('#uploadMask').removeAttr('hidden')
-        let formURL = $('#ttbVideoUploadForm').attr('action')
-        let formData = $('#file').val()
-        
-
-        //   event.preventDefault();
+        $.ajax({
+            type: "GET",
+            url: "https://6bvu1dfzvl.execute-api.ap-south-1.amazonaws.com/test/getsignedurl",
+            crossdomain: true,
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (data, textStatus, jqXHR) {
+                let signedURL = JSON.parse(data.body).signedURL
+                alert(signedURL)
+                let selectedFile = $('#file')[0].files[0]
+                $.ajax({
+                    type: "PUT",
+                    url: signedURL,
+                    contentType: "application/octet-stream",
+                    data: {
+                        file: undefined
+                    },
+                    success: function (data, textStatus, jqXHR) {
+                        alert('everything was OK')
+                    }
+                });
+            }
+        });
+        event.preventDefault();
     })
 }
