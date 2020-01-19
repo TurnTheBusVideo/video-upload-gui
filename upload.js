4de8e5153ec38b1228fb7935b1e506b2b732299e
Prasad Thakur <prasad.thakur@sap.com>
2020-01-18 17:30:15 -0800

    debug

--- upload.js
+++ upload.js
@@ -1,6 +1,36 @@
+
+
 window.onload = event => {
     $('#uploadMask').attr('hidden', true)
-    $('#ttbVideoUploadForm').submit(event => {
+
+    $.ajax({
+        method: "GET",
+        async:true,
+        url: "https://6bvu1dfzvl.execute-api.ap-south-1.amazonaws.com/test/getsignedurl",
+        crossDomain:true,
+        dataType: 'application/json',
+        success: function (data, textStatus, jqXHR) {
+            console.log('data', data);
+            // let signedURL = 'facebook.com'
+            // $.ajax({
+            //     method: "PUT",
+            //     contentType: "application/octet-stream",
+            //     url: signedURL,
+            //     data: formData,
+            //     success: function (data, textStatus, jqXHR) {
+            //         alert('everything was OK');
+            //     }
+            // });
+        }
+    });
+
+
+    $('#submitUploadForm').click(event => {
         $('#uploadMask').removeAttr('hidden')
+        let formURL = $('#ttbVideoUploadForm').attr('action')
+        let formData = $('#file').val()
+        
+
+        //   event.preventDefault();
     })
 }
