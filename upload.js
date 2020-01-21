5a81568ddaa5f9e5e7a1324963f721d3fcdff019
Prasad Thakur <prasad.thakur@sap.com>
2020-01-20 19:55:17 -0800

    params for getsignedurl

--- upload.js
+++ upload.js
@@ -14,6 +14,7 @@ const sendFile = function (event) {
             success: function (data, textStatus, jqXHR) {
                 console.log('data', data)
                 $('#uploadMask').attr('hidden', true)
+                $('#uploadSucc').removeAttr('hidden')
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 setError('Could not upload file, please check console/network logs.')
@@ -25,20 +26,27 @@ const reader = new FileReader()
 reader.onload = sendFile
 window.onload = event => {
     $('#uploadMask').attr('hidden', true)
-
+    $('#uploadError').attr('hidden', true)
+    $('#uploadSucc').attr('hidden', true)
     try {
         $('#submitUploadForm').click(event => {
             $('#uploadMask').removeAttr('hidden')
+            $('#uploadError').attr('hidden', true)
+            $('#uploadSucc').attr('hidden', true)
+            let selectedFile = $('#file')[0].files[0]
             $.ajax({
                 type: "GET",
                 url: "https://6bvu1dfzvl.execute-api.ap-south-1.amazonaws.com/test/getsignedurl",
                 crossdomain: true,
                 contentType: 'application/json',
                 dataType: 'json',
+                data: {
+                    bucket: 'test-turnthebus-upload',
+                    key: 'voila.png'
+                },
                 success: function (data, textStatus, jqXHR) {
                     signedURL = JSON.parse(data.body).signedURL
                     console.log('Signed URL', signedURL)
-                    let selectedFile = $('#file')[0].files[0]
                     reader.readAsBinaryString(selectedFile);
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
